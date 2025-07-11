fec9c561b2b856218257787c7f5eee7b8456abb9
dakshina <mihirvelaga21144@gmail.com>
2025-07-11 22:07:56 +0530

    created the project

--- flutter/georesolve/lib/login.dart
+++ flutter/georesolve/lib/login.dart
@@ -1,10 +1,22 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AuthorityLogin extends StatelessWidget {
   const AuthorityLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Authority Login");
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Welcome to GeoResolve!!!",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0,),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
