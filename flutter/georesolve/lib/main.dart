2157de5e9dc9731e52fa823cc70f9febced5e150
dakshina <mihirvelaga21144@gmail.com>
2025-07-11 21:30:50 +0530

    created a new project and deleted the old one

--- flutter/georesolve/lib/main.dart
+++ flutter/georesolve/lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:georesolve/home.dart';
 import 'package:georesolve/login.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-Future<void> main() async {
-  await Supabase.initialize(
-    url: 'https://fhrlyyjqxnwjjemmptym.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZocmx5eWpxeG53amplbW1wdHltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NjMyMzYsImV4cCI6MjA2NzUzOTIzNn0.GT2xRbKrbFECo656Rzi6vZU_OH7nDkgRhzAo5nS0B2Y',
-  );
+void main(){
   runApp(MyApp());
 }
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -19,8 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {'/': (context) => Home(), '/login': (context) => Login()},
+      initialRoute: '/login/authority',
+      routes: {
+        '/login/authority':(context)=>AuthorityLogin(),
+      },
     );
   }
 }
