8c8bf27efc4d044a4f16ff19c1996f5b14acd09d
dakshina <mihirvelaga21144@gmail.com>
2025-07-12 08:00:00 +0530

    created the authority login page

--- flutter/georesolve/lib/login.dart
+++ flutter/georesolve/lib/login.dart
@@ -5,14 +5,85 @@ class AuthorityLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String? role;
+    List<String> roles = ['role1', 'role2', 'role3'];
+    final roleController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Text(
-              "Welcome to GeoResolve!!!",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0,),
+            Center(
+              child: Text(
+                "Welcome to GeoResolve!!!",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
+              ),
+            ),
+            Row(
+              children: [
+                const SizedBox(width: 150.0,),
+                Image.asset(
+                  'assets/images/logoAuthorityLogin.png',
+                  height: 150.0,
+                ),
+              ],
+            ),
+            const SizedBox(height: 0.0),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 225.0),
+                    child: DropdownButtonFormField(decoration: InputDecoration(border: OutlineInputBorder(),
+                    labelText: "Role"),
+                        items: roles.map((String role){
+                      return DropdownMenuItem(value: role,
+                          child: Text(role));
+                    }).toList(), onChanged: (String? newValue){
+                      role=newValue;
+                    }),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                    controller: emailController,
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      counterText: "",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextField(
+                    controller: passwordController,
+                    maxLength: 50,
+                    obscuringCharacter: "*",
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      counterText: "",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Color(0xFF571760),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                ),
+              ),
+              child: Text("Login", style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
