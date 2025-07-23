0da49f0f30557edc19e25c8e6ca4222c33bb7c98
dakshina <mihirvelaga21144@gmail.com>
2025-07-23 14:09:40 +0530

    added the supabase also for storage buckets

--- flutter/geeresolve/lib/citizen/citizen_problem.dart
+++ flutter/geeresolve/lib/citizen/citizen_problem.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class CitizenProblem extends StatefulWidget {
   const CitizenProblem({super.key});
@@ -9,11 +13,37 @@ class CitizenProblem extends StatefulWidget {
 }
 
 class _CitizenProblemState extends State<CitizenProblem> {
+  File? _imageFile;
+  final _picker = ImagePicker();
+  final supabase=Supabase.instance.client;
+
+  Future<void> _pickfromCamera() async {
+    try {
+      final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+      if (pickedFile != null) {
+        setState(() {
+          _imageFile = File(pickedFile.path);
+        });
+        supabase.storage.from('problems.pics').upload(pickedFile.path, _imageFile!);
+      }
+    } catch (error) {
+      var message = error.toString();
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(message)));
+      }
+    }
+  }
+
   final _auth = firebase_auth.FirebaseAuth.instance;
   Color mainColor = Color(0xFF180E83);
   firebase_auth.User? _user;
   String? displayPicture;
-  String? firstName;
+  final districtController = TextEditingController();
+  final mandalController = TextEditingController();
+  final villageController = TextEditingController();
+  final concernController = TextEditingController();
 
   @override
   void initState() {
@@ -55,10 +85,11 @@ class _CitizenProblemState extends State<CitizenProblem> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton(
-              onPressed: () => Navigator.pushReplacementNamed(
-                context,
-                '/citizen/searchauthorities',
-              ),
+              onPressed: () =>
+                  Navigator.pushReplacementNamed(
+                    context,
+                    '/citizen/searchauthorities',
+                  ),
               child: Column(
                 children: [
                   Image.asset(
@@ -73,7 +104,8 @@ class _CitizenProblemState extends State<CitizenProblem> {
               ),
             ),
             TextButton(
-              onPressed: ()=>Navigator.pushReplacementNamed(context, '/citizen/dashboard'),
+              onPressed: () =>
+                  Navigator.pushReplacementNamed(context, '/citizen/dashboard'),
               child: Column(
                 children: [
                   Image.asset('assets/images/Vector.png', height: 30.0),
@@ -85,14 +117,34 @@ class _CitizenProblemState extends State<CitizenProblem> {
               ),
             ),
             TextButton(
-              onPressed: ()=>Navigator.pushReplacementNamed(context, '/citizen/issue'),
+              onPressed: () =>
+                  Navigator.pushReplacementNamed(context, '/citizen/issue'),
               child: Column(
                 children: [
-                  Image.asset('assets/images/complaint.png',height: 30.0,),
-                  Text("Issue Complaint",style: TextStyle(color: Colors.white,fontSize: 10.0),),
+                  Image.asset('assets/images/complaint.png', height: 30.0),
+                  Text(
+                    "Issue Complaint",
+                    style: TextStyle(color: Colors.white, fontSize: 10.0),
+                  ),
                 ],
               ),
             ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 30.0),
+            Center(
+              child: Text(
+                "Issue Complaint",
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 28.0),
+              ),
+            ),
+            TextButton(onPressed: () async {
+              _pickfromCamera();
+            }, child: Text("Pick from camera"))
           ],
         ),
       ),
